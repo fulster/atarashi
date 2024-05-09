c5bff6805cc6905710cebca411a67e64b18b3ccf
Etienne Sévin <etienne@magic.fr>
2024-05-09 18:07:17 +0200

    modifs wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # Remplacez 'myapp' par le nom de votre module d'application Flask
+from app.app import app  # Remplacez 'myapp' par le nom de votre module d'application Flask
 
 if __name__ == "__main__":
     app.run()
